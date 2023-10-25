8d1d37ad7a5c99d7f04554b30871499ae945211b
AnaliceMarques <analicemarquessl@gmail.com>
2023-10-24 21:07:14 -0300

    initial config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProjectComponent } from './components/project/project.component';
+import { LanguagesComponent } from './components/languages/languages.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { ProfessionalExperienceComponent } from './components/professional-experience/professional-experience.component';
+import { ProfessionalSummaryComponent } from './components/professional-summary/professional-summary.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectComponent,
+    LanguagesComponent,
+    CoursesComponent,
+    ProfessionalExperienceComponent,
+    ProfessionalSummaryComponent,
+    EducationComponent,
+    SkillsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDialogModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/professional-summary/professional-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-summary',
+  templateUrl: './professional-summary.component.html',
+  styleUrls: ['./professional-summary.component.scss']
+})
+export class ProfessionalSummaryComponent {
+
+}
